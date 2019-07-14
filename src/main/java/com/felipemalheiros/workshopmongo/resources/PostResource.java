a9d637af2526b0005b7e52495c6e6096c6434a4f
Felipe Malheiros <felipemalheirosb@gmail.com>
2019-07-14 20:49:40 -0300

    Acrescentando comentarios aos posts

--- src/main/java/com/felipemalheiros/workshopmongo/resources/PostResource.java
+++ src/main/java/com/felipemalheiros/workshopmongo/resources/PostResource.java
@@ -8,8 +8,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.felipemalheiros.workshopmongo.domain.Post;
-import com.felipemalheiros.workshopmongo.domain.User;
-import com.felipemalheiros.workshopmongo.dto.UserDTO;
 import com.felipemalheiros.workshopmongo.service.PostService;
 
 @RestController
